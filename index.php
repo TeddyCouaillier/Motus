43be921d26e0d6522c1efd74e0f5f1a703c07149
Teddy Couaillier <t.couaillier@simplon-charleville.fr>
2019-04-15 13:36:42 +0200

    Final_version

--- index.php
+++ index.php
@@ -14,30 +14,27 @@
 </head>
 <body>
 
-    <div class="row">
-        <div class="col-lg-3"></div>
-        <div class="col">
-            <div class="d-flex flex-column justify-content-center" id="motus" style="height:550px">
-                <div class="d-flex flex-row justify-content-center text-center words">
-                    <div><input type="text" size="1" maxlength="1" class="letter-1"></div>
-                    <div><input type="text" size="1" maxlength="1" class="letter-1"></div>
-                    <div><input type="text" size="1" maxlength="1" class="letter-1"></div>
-                    <div><input type="text" size="1" maxlength="1" class="letter-1"></div>
-                    <div><input type="text" size="1" maxlength="1" class="letter-1"></div>
-                    <div><input type="text" size="1" maxlength="1" class="letter-1"></div>
-                    <div id="enter" onclick="onClick(1)"><i class="fas fa-check"></i></div>
-                </div>
+    <div class="container text-center">
+        <img src="./img/logo.png" class="img-fluid">
 
+    </div>
+
+    <div class="row">
+        <div class="col-lg-2"></div>
+        <div class="col">
+            <div class="d-flex flex-column justify-content-center " id="motus" style="height:850px">
+                <div id="start" class="mx-auto display-4 shadow-lg" onclick="startGame()">GO</div>
                 
+
+
 
             </div>
         </div>
-        <div class="col-lg-3"></div>
+        <div class="col-lg-2"></div>
     </div>
 
 
 
-    
 
     <!--///////////////////////////////////////
     ////        Déclaration des scripts
--- js/main.js
+++ js/main.js
@@ -1,40 +1,59 @@
-function addWord(nb){
+var tab_words = ['injurieux','divulgation','protection','perruque','essentiel','plantation','mitrailleur','seigneur','gymnastique','glandulaire','optimum','stupefaction','ennemi','rechauffer','geometrie','altercation','ordinateur','tableau','mysterieux','sanguinaire','interminable','legerete','orientation','berserker','partager','funeraille','recipient','programme','autoroute','balancer','constitution','degouter','culturel','braquage','tristement','confusion',
+'excitation','federal','capsule','fragment','gigantesque','babillage','raquette','occident','androgyne','adhesif','munition','sauvage','deformer','decapiter','graveleux','obstacle','langage','scatophile','annihiler','magazine','crepuscule','paradoxe','repasser','surpasser','marginal','corrompu'];
+
+var i_alea = parseInt(Math.random()*tab_words.length);
+console.log(tab_words[i_alea]);
+
+function addWord(taille,nb){
     var classDiv = '<div class="d-flex flex-row justify-content-center text-center words animated fadeIn">';
-    for(var i = 1; i<= 6 ; i++){
-        classDiv += ('<div><input type="text" size="1" maxlength="1" class="letter-'+nb+'"></div>');
+    for(var i = 1; i<= taille ; i++){
+        classDiv += ('<div><input type="text" maxlength="1" class="letter-'+nb+'"></div>');
     }
-    classDiv += '<div id="enter" onclick="onClick('+nb+')"><i class="fas fa-check"></i></div></div>';
+    classDiv += '<div id="enter" onclick="onClick('+nb+')">CONFIRMER</div></div>';
     return classDiv;
 }
 
+
 // ADD NEW WORDS
 var motus = document.getElementById('#motus');
-var cpt = 1;
+var cpt = 0;
+var taille = tab_words[i_alea].length;
 
-function addInput(){
+function startGame(){
+    cpt++;
+    $('#motus').append(addWord(taille,cpt));
+    $('#start').remove();
+}
+
+
+
+function addInput(taille){
     cpt++;
     setTimeout(function(){
     if(cpt<=8){
-        $('#motus').append(addWord(cpt));
+        $('#motus').append(addWord(taille,cpt));
     } else {
-        alert('PERDU !');
+        $('#motus').append('<img src="./img/lose.gif" class="mx-auto img-fluid my-3" id="gif_lose"></img>');
+        $('#motus').append('<span class="text-center text-white h3">Résultat : '+tab_words[i_alea]+'</span>');
+
     }
-    },7000);
+    },(taille+1)*700);
 }
 
 // WORDS SEARCH
-var words_s = 'canape';
+var words_s = tab_words[i_alea];
 var words_st = words_s.split('');
 
 // Changement du background, interval de 2s
 function changeColor(val, color,i){
     setTimeout(function(){
         $(val).addClass(color);
-    },(i+1)*1000);
+    },(i+1)*700);
 }
 
 // VERIF DU MOT SAISI
 function onClick(nb){
+    $('#enter').remove();
     // Tableau temporaire pour gerer les lettres mal placées
     words_tmp = words_s.split('');
     var words = '';
@@ -59,7 +78,10 @@ function onClick(nb){
             }
             words_tmp.splice(tmp,1);
         }
-        else {
+
+    }
+    for(i = 0 ; i < words_st.length ; i++){
+        if(letters[i] !== words_st[i]){
             for(var j = 0 ; j < words_tmp.length ; j++){
                 if(letters[i]===words_tmp[j]){
                     changeColor(letters_select[i],"bad",i);
@@ -68,14 +90,28 @@ function onClick(nb){
         }
     }
     if(words !== words_s){
-        addInput();
+        addInput(taille);
     } else {
         // Attends la fin de l'animation pour afficher l'alerte
         setTimeout(function(){
-            alert('WIN!');
-        },7000);
+            $('#motus').append('<img src="./img/win.gif" class="mx-auto img-fluid my-3" id="gif_win"></img>');
+            $('#motus').append('<a class="mx-auto" href="index.php"><button class="btn btn-warning">Restart</button></a>')
+        },(taille+1)*700);
     }
 
 }
 
 
+
+/*
+
+    function wait(ms){
+        var d = new Date();
+        var d2 = null;
+        do{
+            d2 = new Date();
+        } while(d2-d < ms);
+    }
+
+
+*/
